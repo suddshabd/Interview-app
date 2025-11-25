4a33c205abe298b6958aa965307430566d481a70
sudhanshu <shuddhshabd@gmail.com>
2025-11-26 04:32:24 +0530

    injest update

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -12,12 +12,12 @@ const syncUser = inngest.createFunction(
     { event: "clerk/user.created" },
     async ({ event }) => {
         await connectDB()
-        const { id, email_address, first_name, last_name, image_url } = event.data;
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
 
         const newUser = {
             clerkId: id,
-            email: email_address[0]?.email_address,
+            email: email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""}`,
             profileImage: image_url
         }
